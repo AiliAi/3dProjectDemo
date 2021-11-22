ca4d4c4a32a4e109f610178a9f6ee5dd55b99802
AiliAi <rebane@gmail.com>
2021-11-22 21:32:35 +0200

    extra info

--- src/js/matterport.js
+++ src/js/matterport.js
@@ -1,6 +1,6 @@
 import '../styles/main.scss';
 import '../modules/nav-loading.js';
-import * as bootstrap from 'bootstrap';
+import '../modules/tooltip.js';
 
 const model = document.getElementById("mp-model");
 const url = "https://my.matterport.com/show/?m=T32nCkyJRpv";
